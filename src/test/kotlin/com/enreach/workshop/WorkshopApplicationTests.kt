f2bf70a7ca37d557cfb1b9f20c49e7c646dcd300
Martijn Klene <martijn.klene@voiceworks.com>
2020-03-09 11:03:20 +0100

    Added the first needed files

---
+++ src/test/kotlin/com/enreach/workshop/WorkshopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.enreach.workshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
